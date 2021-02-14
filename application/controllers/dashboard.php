72aca7887cc5b46e72c0f79b316ca97abe7180dc
unknown <nurhayatifitriani77@gmail.com>
2021-02-14 12:49:29 +0700

    admin template

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -20,6 +20,7 @@ class dashboard extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('dashboard_v');
+		// $this->load->view('dashboard_v');
+		$this->template->load('template', 'dashboard_v');
 	}
 }
