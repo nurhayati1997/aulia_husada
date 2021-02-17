495f861a0f447cf489107ceffbd6ac19d1d401f7
unknown <nurhayatifitriani77@gmail.com>
2021-02-17 11:23:02 +0700

    move to template premium

--- application/views/template.php
+++ application/views/template.php
@@ -1,51 +1,106 @@
 <!--
 =========================================================
-* Argon Dashboard - v1.2.0
+* Argon Dashboard PRO - v1.2.0
 =========================================================
-* Product Page: https://www.creative-tim.com/product/argon-dashboard
-
-
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-pro
 * Copyright  Creative Tim (http://www.creative-tim.com)
 * Coded by www.creative-tim.com
-
-
-
 =========================================================
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
 <!DOCTYPE html>
 <html>
 
+
+<!-- Mirrored from demos.creative-tim.com/argon-dashboard-pro/pages/dashboards/dashboard.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 17 Feb 2021 03:16:33 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
   <title>Husada Raya</title>
-  <!-- Favicon -->
-  <link rel="icon" href="<?= base_url() ?>assets_admin/img/brand/logo.ico" type="image/png">
+  <!-- Extra details for Live View on GitHub Pages -->
+  <!-- Canonical SEO -->
+  <link rel="canonical" href="https://www.creative-tim.com/product/argon-dashboard-pro" />
+  <!--  Social tags      -->
+  <meta name="keywords" content="dashboard, bootstrap 4 dashboard, bootstrap 4 design, bootstrap 4 system, bootstrap 4, bootstrap 4 uit kit, bootstrap 4 kit, argon, argon ui kit, creative tim, html kit, html css template, web template, bootstrap, bootstrap 4, css3 template, frontend, responsive bootstrap template, bootstrap ui kit, responsive ui kit, argon dashboard">
+  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <!-- Schema.org markup for Google+ -->
+  <meta itemprop="name" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim">
+  <meta itemprop="description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <meta itemprop="image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg">
+  <!-- Twitter Card data -->
+  <meta name="twitter:card" content="product">
+  <meta name="twitter:site" content="@creativetim">
+  <meta name="twitter:title" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim">
+  <meta name="twitter:description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <meta name="twitter:creator" content="@creativetim">
+  <meta name="twitter:image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg">
+  <!-- Open Graph data -->
+  <meta property="fb:app_id" content="655968634437471">
+  <meta property="og:title" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim" />
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content="https://demos.creative-tim.com/argon-dashboard/index.html" />
+  <meta property="og:image" content="../../../../s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg" />
+  <meta property="og:description" content="Start your development with a Dashboard for Bootstrap 4." />
+  <meta property="og:site_name" content="Creative Tim" />
   <!-- Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+  <!-- Favicon -->
+  <link rel="icon" href="<?= base_url() ?>assets_admin/img/brand/logo.ico" type="image/png">
+
   <!-- Icons -->
   <link rel="stylesheet" href="<?= base_url() ?>assets_admin/vendor/nucleo/css/nucleo.css" type="text/css">
-  <link rel="stylesheet" href="<?= base_url() ?>assets_admin/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets_admin/vendor/%40fortawesome/fontawesome-free/css/all.min.css" type="text/css">
   <!-- Page plugins -->
   <link rel="stylesheet" href="<?= base_url() ?>assets_admin/vendor/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>assets_admin/vendor/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>assets_admin/vendor/datatables.net-select-bs4/css/select.bootstrap4.min.css">
   <!-- Argon CSS -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets_admin/css/argon.css?v=1.2.0" type="text/css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets_admin/css/argon.min5438.css?v=1.2.0" type="text/css">
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        '../../../../www.googletagmanager.com/gtm5445.html?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-NKDMSK6');
+  </script>
+  <!-- End Google Tag Manager -->
 </head>
 
 <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <!-- Sidenav -->
   <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
     <div class="scrollbar-inner">
       <!-- Brand -->
-      <div class="sidenav-header  align-items-center">
-        <a class="navbar-brand" href="javascript:void(0)">
+      <div class="sidenav-header  d-flex  align-items-center">
+        <a class="navbar-brand" href="dashboard.html">
           <img src="<?= base_url() ?>assets_admin/img/brand/main_logo.png" class="navbar-brand-img" alt="...">
         </a>
+        <div class=" ml-auto ">
+          <!-- Sidenav toggler -->
+          <div class="sidenav-toggler d-none d-xl-block" data-action="sidenav-unpin" data-target="#sidenav-main">
+            <div class="sidenav-toggler-inner">
+              <i class="sidenav-toggler-line"></i>
+              <i class="sidenav-toggler-line"></i>
+              <i class="sidenav-toggler-line"></i>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="navbar-inner">
         <!-- Collapse -->
@@ -124,29 +179,30 @@
           <!-- Heading -->
           <h6 class="navbar-heading p-0 text-muted">
             <span class="docs-normal">Documentation</span>
+            <span class="docs-mini">D</span>
           </h6>
           <!-- Navigation -->
           <ul class="navbar-nav mb-md-3">
             <!-- <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html" target="_blank">
+              <a class="nav-link" href="../../docs/getting-started/overview.html" target="_blank">
                 <i class="ni ni-spaceship"></i>
                 <span class="nav-link-text">Getting started</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html" target="_blank">
+              <a class="nav-link" href="../../docs/foundation/colors.html" target="_blank">
                 <i class="ni ni-palette"></i>
                 <span class="nav-link-text">Foundation</span>
               </a>
             </li> -->
             <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html" target="_blank">
+              <a class="nav-link" href="../../docs/components/alerts.html" target="_blank">
                 <i class="ni ni-ui-04"></i>
                 <span class="nav-link-text">Master Data</span>
               </a>
             </li>
             <!-- <li class="nav-item">
-              <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/plugins/charts.html" target="_blank">
+              <a class="nav-link" href="../../docs/plugins/charts.html" target="_blank">
                 <i class="ni ni-chart-pie-35"></i>
                 <span class="nav-link-text">Plugins</span>
               </a>
@@ -158,33 +214,33 @@
   </nav>
   <!-- Main content -->
   <div class="main-content" id="panel">
-    <?php echo $contents ?>
-    <!-- Footer -->
-    <footer class="footer pt-0">
+      <?php echo $contents ?>
+      <!-- Footer -->
+      <footer class="footer pt-0">
         <div class="row align-items-center justify-content-lg-between">
           <div class="col-lg-6">
             <div class="copyright text-center  text-lg-left  text-muted">
-              &copy; 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+              &copy; 2020 <a href="https://www.creative-tim.com/" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
             </div>
           </div>
           <div class="col-lg-6">
             <ul class="nav nav-footer justify-content-center justify-content-lg-end">
               <li class="nav-item">
-                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+                <a href="https://www.creative-tim.com/" class="nav-link" target="_blank">Creative Tim</a>
               </li>
               <li class="nav-item">
                 <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
               </li>
               <li class="nav-item">
-                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+                <a href="http://blog.creative-tim.com/" class="nav-link" target="_blank">Blog</a>
               </li>
               <li class="nav-item">
-                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+                <a href="https://www.creative-tim.com/license" class="nav-link" target="_blank">License</a>
               </li>
             </ul>
           </div>
         </div>
-    </footer>
+      </footer>
   </div>
   <!-- Argon Scripts -->
   <!-- Core -->
@@ -196,10 +252,6 @@
   <!-- Optional JS -->
   <script src="<?= base_url() ?>assets_admin/vendor/chart.js/dist/Chart.min.js"></script>
   <script src="<?= base_url() ?>assets_admin/vendor/chart.js/dist/Chart.extension.js"></script>
-  <!-- Argon JS -->
-  <script src="<?= base_url() ?>assets_admin/js/argon.js?v=1.2.0"></script>
-
-  <!-- Table -->
   <script src="<?= base_url() ?>assets_admin/vendor/datatables.net/js/jquery.dataTables.min.js"></script>
   <script src="<?= base_url() ?>assets_admin/vendor/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
   <script src="<?= base_url() ?>assets_admin/vendor/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
@@ -208,6 +260,44 @@
   <script src="<?= base_url() ?>assets_admin/vendor/datatables.net-buttons/js/buttons.flash.min.js"></script>
   <script src="<?= base_url() ?>assets_admin/vendor/datatables.net-buttons/js/buttons.print.min.js"></script>
   <script src="<?= base_url() ?>assets_admin/vendor/datatables.net-select/js/dataTables.select.min.js"></script>
+  <!-- Argon JS -->
+  <script src="<?= base_url() ?>assets_admin/js/argon.min5438.js?v=1.2.0"></script>
+  <!-- Demo JS - remove this in your project -->
+  <script src="<?= base_url() ?>assets_admin/js/demo.min.js"></script>
+  <script>
+    // Facebook Pixel Code Don't Delete
+    ! function(f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function() {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s)
+    }(window,
+      document, 'script', '../../../../connect.facebook.net/en_US/fbevents.js');
+
+    try {
+      fbq('init', '111649226022273');
+      fbq('track', "PageView");
+
+    } catch (err) {
+      console.log('Facebook Track Error:', err);
+    }
+  </script>
+  <noscript>
+    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=111649226022273&amp;ev=PageView&amp;noscript=1" />
+  </noscript>
 </body>
 
+
+<!-- Mirrored from demos.creative-tim.com/argon-dashboard-pro/pages/dashboards/dashboard.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 17 Feb 2021 03:16:46 GMT -->
 </html>
