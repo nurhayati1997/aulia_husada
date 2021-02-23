7d33fdf3be62603e6c4ee599f4c2167adbdf4d99
unknown <nurhayatifitriani77@gmail.com>
2021-02-23 15:23:47 +0700

    landing page

--- application/views/profile_v.php
+++ application/views/profile_v.php
@@ -121,10 +121,11 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-8">
                         <div class="header-hero-content text-center">
+                            <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Selamat Datang</h3>
                             <h3 class="header-sub-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.2s">Praktik Bersama</h3>
-                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">Husada Raya</h2>
-                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Jl.raya Burneh, Bangkalan, Jawa Timur</p>
-                            <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a>
+                            <h2 class="header-title wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.5s">dr.Nunuk Kristriani</h2>
+                            <p class="text wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="0.8s">Jl.raya Kusuma Bangsa Burneh, Bangkalan, Jawa Timur</p>
+                            <!-- <a href="#" class="main-btn wow fadeInUp" data-wow-duration="1.3s" data-wow-delay="1.1s">Get Started</a> -->
                         </div> <!-- header hero content -->
                     </div>
                 </div> <!-- row -->
@@ -142,36 +143,6 @@
     
     <!--====== HEADER PART ENDS ======-->
     
-    <!--====== BRAMD PART START ======-->
-    
-    <div class="brand-area pt-90">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="brand-logo d-flex align-items-center justify-content-center justify-content-md-between">
-                        <div class="single-logo mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
-                            <img src="<?= base_url() ?>assets/images/brand-1.png" alt="brand">
-                        </div> <!-- single logo -->
-                        <div class="single-logo mt-30 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.2s">
-                            <img src="<?= base_url() ?>assets/images/brand-2.png" alt="brand">
-                        </div> <!-- single logo -->
-                        <div class="single-logo mt-30 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.3s">
-                            <img src="<?= base_url() ?>assets/images/brand-3.png" alt="brand">
-                        </div> <!-- single logo -->
-                        <div class="single-logo mt-30 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.4s">
-                            <img src="<?= base_url() ?>assets/images/brand-4.png" alt="brand">
-                        </div> <!-- single logo -->
-                        <div class="single-logo mt-30 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
-                            <img src="<?= base_url() ?>assets/images/brand-5.png" alt="brand">
-                        </div> <!-- single logo -->
-                    </div> <!-- brand logo -->
-                </div>
-            </div>   <!-- row -->
-        </div> <!-- container -->
-    </div>
-    
-    <!--====== BRAMD PART ENDS ======-->
-    
     <!--====== SERVICES PART START ======-->
     
     <section id="features" class="services-area pt-120">
@@ -180,7 +151,7 @@
                 <div class="col-lg-10">
                     <div class="section-title text-center pb-40">
                         <div class="line m-auto"></div>
-                        <h3 class="title">Clean and simple design, <span> Comes with everything you need to get started!</span></h3>
+                        <h3 class="title">Kami Menyediakan Pelayanan <span> Terbaik untuk Kesehatan Anda dan Keluarga</span></h3>
                     </div> <!-- section title -->
                 </div>
             </div> <!-- row -->
@@ -193,9 +164,9 @@
                             <i class="lni-baloon"></i>
                         </div>
                         <div class="services-content mt-30">
-                            <h4 class="services-title"><a href="#">Clean</a></h4>
-                            <p class="text">Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonu eirmod tempor invidunt labore.</p>
-                            <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a>
+                            <h4 class="services-title"><a href="#">dr.Nunuk Kristiani,Sp.Rad</a></h4>
+                            <p class="text">Spesialis Radiologi</p>
+                            <!-- <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a> -->
                         </div>
                     </div> <!-- single services -->
                 </div>
@@ -207,9 +178,9 @@
                             <i class="lni-cog"></i>
                         </div>
                         <div class="services-content mt-30">
-                            <h4 class="services-title"><a href="#">Robust</a></h4>
-                            <p class="text">Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonu eirmod tempor invidunt labore.</p>
-                            <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a>
+                            <h4 class="services-title"><a href="#">dr.Nuryatien Husna,Sp.KFR</a></h4>
+                            <p class="text">Spesialis Kedokteran Fisik & Rehabilitas</p>
+                            <!-- <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a> -->
                         </div>
                     </div> <!-- single services -->
                 </div>
@@ -221,9 +192,9 @@
                             <i class="lni-bolt-alt"></i>
                         </div>
                         <div class="services-content mt-30">
-                            <h4 class="services-title"><a href="#">Powerful</a></h4>
-                            <p class="text">Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonu eirmod tempor invidunt labore.</p>
-                            <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a>
+                            <h4 class="services-title"><a href="#">dr.Mirathi Ayu Irnanda</a></h4>
+                            <p class="text">Dokter Umum</p>
+                            <!-- <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a> -->
                         </div>
                     </div> <!-- single services -->
                 </div>
@@ -242,10 +213,13 @@
                     <div class="about-content mt-50 wow fadeInLeftBig" data-wow-duration="1s" data-wow-delay="0.5s">
                         <div class="section-title">
                             <div class="line"></div>
-                            <h3 class="title">Quick & Easy <span>to Use Bootstrap Template</span></h3>
+                            <h3 class="title">Pemeriksaan <span>Rawat Jalan</span></h3>
                         </div> <!-- section title -->
-                        <p class="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, seiam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing.</p>
-                        <a href="#" class="main-btn">Try it Free</a>
+                        <p class="text">Merupakan layanan terhadap pasien yang masuk untuk keperluan observasi, 
+                            diagnosa, pengobatan, rehabilitasi medik dan pelayanan kesehatan lainnya tanpa tinggal 
+                            di ruang rawat inap. 
+                        </p>
+                        <!-- <a href="#" class="main-btn">Try it Free</a> -->
                     </div> <!-- about content -->
                 </div>
                 <div class="col-lg-6">
@@ -274,10 +248,11 @@
                     <div class="about-content mt-50 wow fadeInLeftBig" data-wow-duration="1s" data-wow-delay="0.5s">
                         <div class="section-title">
                             <div class="line"></div>
-                            <h3 class="title">Modern design <span> with Essential Features</span></h3>
+                            <h3 class="title">Pemeriksaan Penunjang <span> USG & EKG</span></h3>
                         </div> <!-- section title -->
-                        <p class="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, seiam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing.</p>
-                        <a href="#" class="main-btn">Try it Free</a>
+                        <p class="text">Merupakan Layanan Pemeriksaan Penunjang untuk memonitor kondisi pasien dan 
+                        menentukan langkah penanganan yang tepat serta memantau keberhasilan terapi pada pasien</p>
+                        <!-- <a href="#" class="main-btn">Try it Free</a> -->
                     </div> <!-- about content -->
                 </div>
                 <div class="col-lg-6 order-lg-first">
@@ -299,10 +274,10 @@
                     <div class="about-content mt-50 wow fadeInLeftBig" data-wow-duration="1s" data-wow-delay="0.5s">
                         <div class="section-title">
                             <div class="line"></div>
-                            <h3 class="title"><span>Crafted for</span> SaaS, App and Software Landing Page</h3>
+                            <h3 class="title"><span>Fasilitas</span> Laboratoium & Apotik</h3>
                         </div> <!-- section title -->
-                        <p class="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, seiam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing.</p>
-                        <a href="#" class="main-btn">Try it Free</a>
+                        <p class="text">Menyediakan Fasilitas lengkap seperti Laboratorium dan Apotik</p>
+                        <!-- <a href="#" class="main-btn">Try it Free</a> -->
                     </div> <!-- about content -->
                 </div>
                 <div class="col-lg-6">
