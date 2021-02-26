c9d94cd41c528ba32ca3b6e9448c4299797091a1
unknown <nurhayatifitriani77@gmail.com>
2021-02-26 10:43:33 +0700

    bentrok

--- application/views/laporan_v.php
+++ application/views/laporan_v.php
@@ -1,5 +1,4 @@
 <div class="content">
-<<<<<<< HEAD
     <!-- Header -->
     <!-- Header -->
     <div class="header bg-primary pb-6">
@@ -19,27 +18,6 @@
             <div class="col-lg-6 col-5 text-right">
               <a href="#" class="btn btn-sm btn-neutral">Print Laporan</a>
             </div>
-=======
-  <!-- Header -->
-  <!-- Header -->
-  <div class="header bg-primary pb-6">
-    <div class="container-fluid">
-      <div class="header-body">
-        <div class="row align-items-center py-4">
-          <div class="col-lg-6 col-7">
-            <h6 class="h2 text-white d-inline-block mb-0">Husada Raya</h6>
-            <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
-              <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
-                <li class="breadcrumb-item"><a><i class="fas fa-home"></i></a></li>
-                <li class="breadcrumb-item"><a>Laporan</a></li>
-                <!-- <li class="breadcrumb-item active" aria-current="page">Default</li> -->
-              </ol>
-            </nav>
-          </div>
-          <div class="col-lg-6 col-5 text-right">
-            <a href="#" class="btn btn-sm btn-neutral">Print Laporan</a>
->>>>>>> 1a6f557cd669ddb45ef8c1addfe2df632ff17e3b
-          </div>
         </div>
       </div>
     </div>
