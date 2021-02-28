9966d5f9f285161a51166aede8992ef0163ecda5
Ainur Inas Annisa <150411100016@trunojoyo.ac.id>
2021-02-28 20:29:29 +0700

    session

--- application/controllers/resume_medis.php
+++ application/controllers/resume_medis.php
@@ -7,9 +7,9 @@ class resume_medis extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
-		// if (!$this->session->userdata("id_user")) {
-		// 	redirect("login");
-		// }
+		if (!$this->session->userdata("id_user")) {
+			redirect("login");
+		}
 		$this->load->model('Db_model');
 		$this->load->helper('file');
 	}
