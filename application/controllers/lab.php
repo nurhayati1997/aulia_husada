0574a067c1cbc9e555c53ee834a0f4e0783c5737
Ainur Inas Annisa <150411100016@trunojoyo.ac.id>
2021-03-20 22:33:30 +0700

    lab

--- application/controllers/lab.php
+++ application/controllers/lab.php
@@ -16,4 +16,32 @@ class lab extends CI_Controller
 	{
 		$this->template->load('template', 'lab_v');
 	}
+
+	function tambah()
+	{
+		$time = strtotime($this->input->post('tgl', TRUE));
+		$newformat = date('Y-m-d', $time);
+		$data = [
+			"nama" => $this->input->post('nama', TRUE),
+			"jenis" => $this->input->post('jenis', TRUE),
+			"tgl" => $newformat,
+			"pengirim" => $this->input->post('pengirim', TRUE),
+			"diskon" => $this->input->post('disc', TRUE),
+			"tarif" => $this->input->post('tarif', TRUE)
+		];
+
+		// echo json_encode($data);
+		echo json_encode($this->db_model->insert_get("tbl_lab", $data));
+	}
+
+	function get_lab()
+	{
+		echo json_encode($this->db_model->get_where('tbl_lab', array('tgl' => strval(date('Y-m-d'))))->result());
+		// echo json_encode(strval(date('Y-m-d')));
+	}
+
+	function hapus(){
+		// echo json_encode($this->input->post('id', TRUE));
+		echo json_encode($this->db_model->delete('tbl_lab', ["id_lab" => $this->input->post('id', TRUE)]));
+	}
 }
--- application/views/lab_v.php
+++ application/views/lab_v.php
@@ -51,6 +51,7 @@
                               <th>Harga</th>
                               <th>Discount</th>
                               <th>Total</th>
+                              <th></th>
                           </tr>
                       </thead>
                       <tfoot>
@@ -61,6 +62,7 @@
                               <th>Harga</th>
                               <th>Discount</th>
                               <th>Total</th>
+                              <th></th>
                           </tr>
                       </tfoot>
                       <tbody>
@@ -135,7 +137,7 @@
                   <div class="input-group-prepend">
                     <span class="input-group-text"><i class="ni ni-box-2"></i></span>
                   </div>
-                  <input class="form-control" placeholder="Tarif" id="tarif" type="number">
+                  <input class="form-control" placeholder="Tarif" id="tarif" type="text">
                 </div>
               </div>
             </div>
@@ -161,6 +163,31 @@
 </div>
 
 <script>
+  $(document).ready(function() {
+    ambil_data();
+    var rupiah = document.getElementById('tarif');
+		rupiah.addEventListener('keyup', function(e){
+			rupiah.value = formatRupiah(this.value, 'Rp. ');
+		});
+  });
+
+  function formatRupiah(angka, prefix){
+    var number_string = angka.replace(/[^,\d]/g, '').toString(),
+    split   		= number_string.split(','),
+    sisa     		= split[0].length % 3,
+    rupiah     		= split[0].substr(0, sisa),
+    ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
+
+    // tambahkan titik jika yang di input sudah menjadi angka ribuan
+    if(ribuan){
+      separator = sisa ? '.' : '';
+      rupiah += separator + ribuan.join('.');
+    }
+
+    rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+    return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+  }
+
   function tampil_daftar_modal(){
     reset_form();
     $('#modal-default').modal('show');
@@ -174,5 +201,172 @@
     document.getElementById('pengirim').value = "";
     document.getElementById('tarif').value = "";
     document.getElementById('disc').value = "";
+  }
+
+  function ambil_data(){
+    $('#datatable-basic').DataTable({
+      destroy: true,
+      "order": [[ 1, "asc" ]],
+        "ajax": {
+        "type": "POST",
+          "url": "<?php echo site_url("lab/get_lab") ?>",
+          "dataSrc": ""
+      },
+      "columns": [
+          {
+              "data": "nama"
+          },
+          {
+              "data": "jenis"
+          },
+          {
+              "data": "pengirim"
+          },
+          {
+              "data": "tarif",
+              "render": function(data, type, row) {
+                return formatRupiah(data, 'Rp. ')
+              }
+          },
+          {
+              "data": "diskon",
+              "render": function(data, type, row) {
+                return data + " %"
+              }
+          },
+          {
+              "data": "tarif",
+              "render": function(data, type, row) {
+                var harga = data;
+                var diskon = data * row.diskon / 100;
+                return formatRupiah(parseInt(harga-diskon).toString(), 'Rp. ')
+              }
+          },
+          {
+              "data": "id_lab",
+              "render": function(data, type, row) {
+                return '<div style="cursor:pointer;" title="hapus?" class="badge badge-danger" id="' + data + '" onClick="hapus(' + data + ')"><i class="fa fa-times"></i></div>'
+              }
+          },
+          
+      ]
+    });
+  }
+
+  function simpan(){
+    if(document.getElementById('nama').value == ""){
+      document.getElementById('nama').focus();
+    }else if(document.getElementById('jenis').value == ""){
+      document.getElementById('jenis').focus();
+    }else if(document.getElementById('tgl').value == ""){
+      document.getElementById('tgl').focus();
+    }else if(document.getElementById('pengirim').value == ""){
+      document.getElementById('pengirim').focus();
+    }else if(document.getElementById('tarif').value == ""){
+      document.getElementById('tarif').focus();
+    }else if(document.getElementById('disc').value == ""){
+      document.getElementById('disc').focus();
+    }else{
+      var form_data = new FormData();
+      form_data.append('nama', document.getElementById("nama").value);
+      form_data.append('jenis', document.getElementById("jenis").value);
+      form_data.append('tgl', document.getElementById("tgl").value);
+
+      form_data.append('pengirim', document.getElementById("pengirim").value);
+      form_data.append('disc', document.getElementById("disc").value);
+
+      var tarif = document.getElementById("tarif").value;
+      tarif = tarif.replace("Rp. ", "");
+      tarif = tarif.replace(".", "");
+
+      form_data.append('tarif', parseInt(tarif));
+
+      $.ajax({
+          type: 'POST',
+          data: form_data,
+          url: '<?= base_url() ?>lab/tambah',
+          processData:false,
+          contentType:false,
+          cache:false,
+          dataType: 'json',
+          beforeSend: function () {
+            $('#simpan_button').attr('disabled', true);
+            $('#loader').html('');
+            addSpinner($('#loader'));
+          },
+          success: function(data) {
+            // console.log(data);
+            ambil_data();
+            $('#modal-default').modal('hide');
+
+            $('#simpan_button').attr('disabled', false);
+            removeSpinner($('#loader'), function () {
+              $('#loader').html('');
+            });
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Data Berhasil Ditambahkan',
+              showConfirmButton: false,
+              timer: 2500
+            });
+          }
+        });
+    }
+  }
+
+  function hapus(id){
+    Swal.fire({
+      title: 'Hapus ?',
+      text: "Data yang telkah dihapus tidak dapat dikembalikan!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Hapus'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        $.ajax({
+          type: 'POST',
+          url: '<?= base_url() ?>lab/hapus',
+          dataType: 'json',
+          data: 'id=' + id,
+          success: function(data) {
+            ambil_data();
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Data Berhasil Dihapus',
+              showConfirmButton: false,
+              timer: 1500
+            })
+          }
+        });
+      }
+    })
+  }
+
+  function addSpinner(el, static_pos)
+  {
+    var spinner = el.children('.spinner');
+    if (spinner.length && !spinner.hasClass('spinner-remove')) return null;
+    !spinner.length && (spinner = $('<div class="spinner' + (static_pos ? '' : ' spinner-absolute') + '"/>').appendTo(el));
+    animateSpinner(spinner, 'add');
+  }
+
+  function removeSpinner(el, complete)
+  {
+    var spinner = el.children('.spinner');
+    spinner.length && animateSpinner(spinner, 'remove', complete);
+  }
+
+  function animateSpinner(el, animation, complete)
+  {
+    if (el.data('animating')) {
+      el.removeClass(el.data('animating')).data('animating', null);
+      el.data('animationTimeout') && clearTimeout(el.data('animationTimeout'));
+    }
+    el.addClass('spinner-' + animation).data('animating', 'spinner-' + animation);
+    el.data('animationTimeout', setTimeout(function() { animation == 'remove' && el.remove(); complete && complete(); }, parseFloat(el.css('animation-duration')) * 1000));
   }
 </script>
