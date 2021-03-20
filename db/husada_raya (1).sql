82da54284b2717da6a5491517719de86c6aa9dc0
Ainur Inas Annisa <150411100016@trunojoyo.ac.id>
2021-03-20 22:34:55 +0700

    db yes

--- db/husada_raya (1).sql
+++ db/husada_raya (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Mar 2021 pada 10.20
--- Versi server: 10.4.11-MariaDB
--- Versi PHP: 7.4.4
+-- Waktu pembuatan: 20 Mar 2021 pada 16.34
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -85,6 +85,29 @@ INSERT INTO `tbl_kecamatan` (`ID`, `KODE_KECAMATAN`, `KECAMATAN`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tbl_lab`
+--
+
+CREATE TABLE `tbl_lab` (
+  `id_lab` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jenis` varchar(100) NOT NULL,
+  `tgl` date NOT NULL,
+  `pengirim` varchar(100) NOT NULL,
+  `tarif` int(11) NOT NULL,
+  `diskon` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_lab`
+--
+
+INSERT INTO `tbl_lab` (`id_lab`, `nama`, `jenis`, `tgl`, `pengirim`, `tarif`, `diskon`) VALUES
+(2, 'q', 'q', '2021-03-20', 'qwq', 20000, 10);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tbl_pasien`
 --
 
@@ -150,7 +173,7 @@ CREATE TABLE `tbl_riwayat_diagnosa` (
 --
 
 INSERT INTO `tbl_riwayat_diagnosa` (`id_diagnosa`, `id_antrian`, `keluhan`, `penyakit_sekarang`, `penyakit_dahulu`, `riwayat_alergi`, `riwayat_operasi`, `riwayat_transfusi`, `riwayat_obat`, `kesadaran_umum`, `kesadaran`, `tekanan_darah`, `nadi`, `suhu`, `rr`, `diagnosa`, `terapi`, `hasil_lab`, `hasil_radiologi`, `hasil_catatan`, `tindakan`, `berkas_tindakan`) VALUES
-(1, 1, 'rindu', '', '', '', '', '', '', '', '', '11', '13', '12', '31', 'Butuh Kasih Sayang', 'NIKAH', NULL, NULL, NULL, 0, '2021-02-25_1_inas1.pdf'),
+(1, 1, 'rindu', '', '', '', '', '', '', '', '', '11', '13', '12', '31', 'Butuh Kasih Sayang', 'NIKAH', NULL, NULL, NULL, 1, '2021-02-25_1_inas.pdf'),
 (2, 2, 'test', '', '', '', '', '', '', '', '', '12', '12', '12', '11', '11', '11', NULL, NULL, NULL, NULL, NULL),
 (3, 4, 'test', '', '', '', '', '', '', '', '', '12', '12', '12', '12', '11', '11', '2021-02-28_3_inas.pdf', NULL, NULL, NULL, NULL),
 (4, 3, 'test', '', '', '', '', '', '', '', '', '12', '12', '12', '12', 'test', 'test', '2021-02-28_4_inas.pdf', '2021-02-28_4_inas2.jpeg', '2021-02-28_4_inas.jpg', NULL, NULL);
@@ -218,7 +241,35 @@ INSERT INTO `tbl_user` (`id_user`, `nama`, `jabatan`, `email`, `password`, `rule
 (1, 'dr.Nunuk Kristiani,Sp.Rad', 'Spesialis Radiologi', 'test@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 3, 0),
 (2, 'dr.Nuryatien Husna,Sp.KFR', 'Spesialis Kedokteran Fisik & Rehabilitas', 'test@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 3, 0),
 (3, 'dr.Mirathi Ayu Irnanda', 'Dokter Umum', 'test@gmail.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 3, 0),
-(4, 'inas', 'admin', 'ainur.inas.annisa@gmail.com', '$y201$iiR$XdtHzKvDqiKDI8T1kQM3d2UJIuZs1yaR1JvhF0fw4BazAyKERI', 2, 0);
+(4, 'inas', 'admin', 'ainur.inas.annisa@gmail.com', '$y201$iiR$XdtHzKvDqiKDI8T1kQM3d2UJIuZs1yaR1JvhF0fw4BazAyKERI', 2, 1),
+(5, 'inas', 'admin', 'ainur.inas.annisa97@gmail.com', '$y201$PQQ$UEjO3lmWWD0RzGG4ML1lFz/TteUF5b.5ForZg82wnVxT5GSxrD', 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_usg`
+--
+
+CREATE TABLE `tbl_usg` (
+  `id_usg` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `tgl` date NOT NULL,
+  `telp` int(11) NOT NULL,
+  `pengirim` varchar(100) NOT NULL,
+  `diagnosa` varchar(250) NOT NULL,
+  `usia` int(11) NOT NULL,
+  `tarif` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_usg`
+--
+
+INSERT INTO `tbl_usg` (`id_usg`, `nama`, `alamat`, `tgl`, `telp`, `pengirim`, `diagnosa`, `usia`, `tarif`) VALUES
+(2, 'b', 'b', '2021-03-20', 12, 'qw', 'qw', 12, 12121),
+(3, 'qq', 'q', '2021-03-20', 12, '1qw', 'qw', 1212, 12000),
+(4, 'q', 'qq', '2021-03-20', 1212, 'asd', 'asd', 1212, 12000);
 
 -- --------------------------------------------------------
 
@@ -371,7 +422,7 @@ CREATE TABLE `v_riwayat_diagnosa` (
 --
 DROP TABLE IF EXISTS `vw_belum_bayar`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_belum_bayar`  AS  select `tbl_antrian`.`id_antrian` AS `id_antrian`,`tbl_antrian`.`id_pasien` AS `id_pasien`,`tbl_antrian`.`id_user` AS `id_user`,`tbl_pasien`.`nama` AS `nama`,`tbl_pasien`.`ortu` AS `ortu`,`tbl_pasien`.`nik` AS `nik`,`tbl_pasien`.`kode` AS `kode` from (`tbl_antrian` join `tbl_pasien` on(`tbl_antrian`.`id_pasien` = `tbl_pasien`.`id`)) where !exists(select 1 from `tbl_transaksi` where `tbl_antrian`.`id_antrian` = `tbl_transaksi`.`id_antrian` limit 1) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_belum_bayar`  AS SELECT `tbl_antrian`.`id_antrian` AS `id_antrian`, `tbl_antrian`.`id_pasien` AS `id_pasien`, `tbl_antrian`.`id_user` AS `id_user`, `tbl_pasien`.`nama` AS `nama`, `tbl_pasien`.`ortu` AS `ortu`, `tbl_pasien`.`nik` AS `nik`, `tbl_pasien`.`kode` AS `kode` FROM (`tbl_antrian` join `tbl_pasien` on(`tbl_antrian`.`id_pasien` = `tbl_pasien`.`id`)) WHERE !exists(select 1 from `tbl_transaksi` where `tbl_antrian`.`id_antrian` = `tbl_transaksi`.`id_antrian` limit 1) ;
 
 -- --------------------------------------------------------
 
@@ -380,7 +431,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `vw_transaksi`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_transaksi`  AS  select `tbl_tindakan`.`id_tindakan` AS `id_tindakan`,`tbl_tindakan`.`nama_tindakan` AS `nama_tindakan`,`tbl_tindakan`.`harga` AS `harga`,`tbl_transaksi`.`id_transaksi` AS `id_transaksi`,`tbl_transaksi`.`tanggal` AS `tanggal`,`tbl_transaksi`.`id_pasien` AS `id_pasien`,`tbl_transaksi`.`dokter` AS `dokter`,`tbl_user`.`nama` AS `namaUser`,`tbl_user`.`rule` AS `rule`,`tbl_pasien`.`nama` AS `namaPasien`,`tbl_pasien`.`ortu` AS `ortu` from ((((`tbl_transaksi_tindakan` join `tbl_transaksi` on(`tbl_transaksi_tindakan`.`id_transaksi` = `tbl_transaksi`.`id_transaksi`)) join `tbl_tindakan` on(`tbl_tindakan`.`id_tindakan` = `tbl_transaksi_tindakan`.`id_tindakan`)) join `tbl_user` on(`tbl_user`.`id_user` = `tbl_transaksi`.`dokter`)) join `tbl_pasien` on(`tbl_pasien`.`id` = `tbl_transaksi`.`id_pasien`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_transaksi`  AS SELECT `tbl_tindakan`.`id_tindakan` AS `id_tindakan`, `tbl_tindakan`.`nama_tindakan` AS `nama_tindakan`, `tbl_tindakan`.`harga` AS `harga`, `tbl_transaksi`.`id_transaksi` AS `id_transaksi`, `tbl_transaksi`.`tanggal` AS `tanggal`, `tbl_transaksi`.`id_pasien` AS `id_pasien`, `tbl_transaksi`.`dokter` AS `dokter`, `tbl_user`.`nama` AS `namaUser`, `tbl_user`.`rule` AS `rule`, `tbl_pasien`.`nama` AS `namaPasien`, `tbl_pasien`.`ortu` AS `ortu` FROM ((((`tbl_transaksi_tindakan` join `tbl_transaksi` on(`tbl_transaksi_tindakan`.`id_transaksi` = `tbl_transaksi`.`id_transaksi`)) join `tbl_tindakan` on(`tbl_tindakan`.`id_tindakan` = `tbl_transaksi_tindakan`.`id_tindakan`)) join `tbl_user` on(`tbl_user`.`id_user` = `tbl_transaksi`.`dokter`)) join `tbl_pasien` on(`tbl_pasien`.`id` = `tbl_transaksi`.`id_pasien`)) ;
 
 -- --------------------------------------------------------
 
@@ -389,7 +440,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `v_antrian`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_antrian`  AS  select `tbl_antrian`.`id_antrian` AS `id_antrian`,`tbl_antrian`.`id_pasien` AS `id_pasien`,`v_pasien`.`kode` AS `kode`,`v_pasien`.`nik` AS `nik`,`v_pasien`.`nama` AS `nama`,`v_pasien`.`ortu` AS `ortu`,`v_pasien`.`alamat` AS `alamat`,`v_pasien`.`nohp` AS `nohp`,`v_pasien`.`ttl` AS `ttl`,`v_pasien`.`id_kecamatan` AS `id_kecamatan`,`v_pasien`.`nama_kecamatan` AS `nama_kecamatan`,`v_pasien`.`jenis_kelamin` AS `jenis_kelamin`,`v_pasien`.`agama` AS `agama`,`v_pasien`.`status` AS `status`,`v_pasien`.`pekerjaan` AS `pekerjaan`,`v_pasien`.`pendidikan` AS `pendidikan`,`v_pasien`.`tgl_input` AS `tgl_input`,`tbl_antrian`.`id_user` AS `id_dokter`,`tbl_user`.`nama` AS `nama_dokter`,`tbl_user`.`jabatan` AS `spesialis`,`tbl_antrian`.`tanggal_antri` AS `tanggal_antri`,`tbl_antrian`.`status` AS `status_antrian` from ((`tbl_antrian` left join `v_pasien` on(`v_pasien`.`id` = `tbl_antrian`.`id_pasien`)) left join `tbl_user` on(`tbl_user`.`id_user` = `tbl_antrian`.`id_user`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_antrian`  AS SELECT `tbl_antrian`.`id_antrian` AS `id_antrian`, `tbl_antrian`.`id_pasien` AS `id_pasien`, `v_pasien`.`kode` AS `kode`, `v_pasien`.`nik` AS `nik`, `v_pasien`.`nama` AS `nama`, `v_pasien`.`ortu` AS `ortu`, `v_pasien`.`alamat` AS `alamat`, `v_pasien`.`nohp` AS `nohp`, `v_pasien`.`ttl` AS `ttl`, `v_pasien`.`id_kecamatan` AS `id_kecamatan`, `v_pasien`.`nama_kecamatan` AS `nama_kecamatan`, `v_pasien`.`jenis_kelamin` AS `jenis_kelamin`, `v_pasien`.`agama` AS `agama`, `v_pasien`.`status` AS `status`, `v_pasien`.`pekerjaan` AS `pekerjaan`, `v_pasien`.`pendidikan` AS `pendidikan`, `v_pasien`.`tgl_input` AS `tgl_input`, `tbl_antrian`.`id_user` AS `id_dokter`, `tbl_user`.`nama` AS `nama_dokter`, `tbl_user`.`jabatan` AS `spesialis`, `tbl_antrian`.`tanggal_antri` AS `tanggal_antri`, `tbl_antrian`.`status` AS `status_antrian` FROM ((`tbl_antrian` left join `v_pasien` on(`v_pasien`.`id` = `tbl_antrian`.`id_pasien`)) left join `tbl_user` on(`tbl_user`.`id_user` = `tbl_antrian`.`id_user`)) ;
 
 -- --------------------------------------------------------
 
@@ -398,7 +449,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `v_pasien`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_pasien`  AS  select `tbl_pasien`.`id` AS `id`,`tbl_pasien`.`kode` AS `kode`,`tbl_pasien`.`nik` AS `nik`,`tbl_pasien`.`nama` AS `nama`,`tbl_pasien`.`ortu` AS `ortu`,`tbl_pasien`.`alamat` AS `alamat`,`tbl_pasien`.`nohp` AS `nohp`,`tbl_pasien`.`ttl` AS `ttl`,`tbl_pasien`.`kecamatan` AS `id_kecamatan`,`tbl_kecamatan`.`KECAMATAN` AS `nama_kecamatan`,`tbl_pasien`.`jenis_kelamin` AS `jenis_kelamin`,`tbl_pasien`.`agama` AS `agama`,`tbl_pasien`.`status` AS `status`,`tbl_pasien`.`pekerjaan` AS `pekerjaan`,`tbl_pasien`.`pendidikan` AS `pendidikan`,`tbl_pasien`.`tgl_input` AS `tgl_input` from (`tbl_pasien` left join `tbl_kecamatan` on(`tbl_kecamatan`.`ID` = `tbl_pasien`.`kecamatan`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_pasien`  AS SELECT `tbl_pasien`.`id` AS `id`, `tbl_pasien`.`kode` AS `kode`, `tbl_pasien`.`nik` AS `nik`, `tbl_pasien`.`nama` AS `nama`, `tbl_pasien`.`ortu` AS `ortu`, `tbl_pasien`.`alamat` AS `alamat`, `tbl_pasien`.`nohp` AS `nohp`, `tbl_pasien`.`ttl` AS `ttl`, `tbl_pasien`.`kecamatan` AS `id_kecamatan`, `tbl_kecamatan`.`KECAMATAN` AS `nama_kecamatan`, `tbl_pasien`.`jenis_kelamin` AS `jenis_kelamin`, `tbl_pasien`.`agama` AS `agama`, `tbl_pasien`.`status` AS `status`, `tbl_pasien`.`pekerjaan` AS `pekerjaan`, `tbl_pasien`.`pendidikan` AS `pendidikan`, `tbl_pasien`.`tgl_input` AS `tgl_input` FROM (`tbl_pasien` left join `tbl_kecamatan` on(`tbl_kecamatan`.`ID` = `tbl_pasien`.`kecamatan`)) ;
 
 -- --------------------------------------------------------
 
@@ -407,7 +458,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `v_riwayat_diagnosa`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_riwayat_diagnosa`  AS  select `tbl_riwayat_diagnosa`.`id_diagnosa` AS `id_diagnosa`,`v_antrian`.`id_antrian` AS `id_antrian`,`v_antrian`.`id_pasien` AS `id_pasien`,`v_antrian`.`kode` AS `kode`,`v_antrian`.`nik` AS `nik`,`v_antrian`.`nama` AS `nama`,`v_antrian`.`ortu` AS `ortu`,`v_antrian`.`alamat` AS `alamat`,`v_antrian`.`nohp` AS `nohp`,`v_antrian`.`ttl` AS `ttl`,`v_antrian`.`id_kecamatan` AS `id_kecamatan`,`v_antrian`.`nama_kecamatan` AS `nama_kecamatan`,`v_antrian`.`jenis_kelamin` AS `jenis_kelamin`,`v_antrian`.`agama` AS `agama`,`v_antrian`.`status` AS `status`,`v_antrian`.`pekerjaan` AS `pekerjaan`,`v_antrian`.`pendidikan` AS `pendidikan`,`v_antrian`.`tgl_input` AS `tgl_input`,`v_antrian`.`id_dokter` AS `id_dokter`,`v_antrian`.`nama_dokter` AS `nama_dokter`,`v_antrian`.`spesialis` AS `spesialis`,`v_antrian`.`tanggal_antri` AS `tanggal_antri`,`v_antrian`.`status_antrian` AS `status_antrian`,`tbl_riwayat_diagnosa`.`keluhan` AS `keluhan`,`tbl_riwayat_diagnosa`.`penyakit_sekarang` AS `penyakit_sekarang`,`tbl_riwayat_diagnosa`.`penyakit_dahulu` AS `penyakit_dahulu`,`tbl_riwayat_diagnosa`.`riwayat_alergi` AS `riwayat_alergi`,`tbl_riwayat_diagnosa`.`riwayat_operasi` AS `riwayat_operasi`,`tbl_riwayat_diagnosa`.`riwayat_transfusi` AS `riwayat_transfusi`,`tbl_riwayat_diagnosa`.`riwayat_obat` AS `riwayat_obat`,`tbl_riwayat_diagnosa`.`kesadaran_umum` AS `kesadaran_umum`,`tbl_riwayat_diagnosa`.`kesadaran` AS `kesadaran`,`tbl_riwayat_diagnosa`.`tekanan_darah` AS `tekanan_darah`,`tbl_riwayat_diagnosa`.`nadi` AS `nadi`,`tbl_riwayat_diagnosa`.`suhu` AS `suhu`,`tbl_riwayat_diagnosa`.`rr` AS `rr`,`tbl_riwayat_diagnosa`.`diagnosa` AS `diagnosa`,`tbl_riwayat_diagnosa`.`terapi` AS `terapi`,`tbl_riwayat_diagnosa`.`hasil_lab` AS `hasil_lab`,`tbl_riwayat_diagnosa`.`hasil_radiologi` AS `hasil_radiologi`,`tbl_riwayat_diagnosa`.`hasil_catatan` AS `hasil_catatan`,`tbl_riwayat_diagnosa`.`tindakan` AS `tindakan`,`tbl_riwayat_diagnosa`.`berkas_tindakan` AS `berkas_tindakan` from (`v_antrian` left join `tbl_riwayat_diagnosa` on(`tbl_riwayat_diagnosa`.`id_antrian` = `v_antrian`.`id_antrian`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_riwayat_diagnosa`  AS SELECT `tbl_riwayat_diagnosa`.`id_diagnosa` AS `id_diagnosa`, `v_antrian`.`id_antrian` AS `id_antrian`, `v_antrian`.`id_pasien` AS `id_pasien`, `v_antrian`.`kode` AS `kode`, `v_antrian`.`nik` AS `nik`, `v_antrian`.`nama` AS `nama`, `v_antrian`.`ortu` AS `ortu`, `v_antrian`.`alamat` AS `alamat`, `v_antrian`.`nohp` AS `nohp`, `v_antrian`.`ttl` AS `ttl`, `v_antrian`.`id_kecamatan` AS `id_kecamatan`, `v_antrian`.`nama_kecamatan` AS `nama_kecamatan`, `v_antrian`.`jenis_kelamin` AS `jenis_kelamin`, `v_antrian`.`agama` AS `agama`, `v_antrian`.`status` AS `status`, `v_antrian`.`pekerjaan` AS `pekerjaan`, `v_antrian`.`pendidikan` AS `pendidikan`, `v_antrian`.`tgl_input` AS `tgl_input`, `v_antrian`.`id_dokter` AS `id_dokter`, `v_antrian`.`nama_dokter` AS `nama_dokter`, `v_antrian`.`spesialis` AS `spesialis`, `v_antrian`.`tanggal_antri` AS `tanggal_antri`, `v_antrian`.`status_antrian` AS `status_antrian`, `tbl_riwayat_diagnosa`.`keluhan` AS `keluhan`, `tbl_riwayat_diagnosa`.`penyakit_sekarang` AS `penyakit_sekarang`, `tbl_riwayat_diagnosa`.`penyakit_dahulu` AS `penyakit_dahulu`, `tbl_riwayat_diagnosa`.`riwayat_alergi` AS `riwayat_alergi`, `tbl_riwayat_diagnosa`.`riwayat_operasi` AS `riwayat_operasi`, `tbl_riwayat_diagnosa`.`riwayat_transfusi` AS `riwayat_transfusi`, `tbl_riwayat_diagnosa`.`riwayat_obat` AS `riwayat_obat`, `tbl_riwayat_diagnosa`.`kesadaran_umum` AS `kesadaran_umum`, `tbl_riwayat_diagnosa`.`kesadaran` AS `kesadaran`, `tbl_riwayat_diagnosa`.`tekanan_darah` AS `tekanan_darah`, `tbl_riwayat_diagnosa`.`nadi` AS `nadi`, `tbl_riwayat_diagnosa`.`suhu` AS `suhu`, `tbl_riwayat_diagnosa`.`rr` AS `rr`, `tbl_riwayat_diagnosa`.`diagnosa` AS `diagnosa`, `tbl_riwayat_diagnosa`.`terapi` AS `terapi`, `tbl_riwayat_diagnosa`.`hasil_lab` AS `hasil_lab`, `tbl_riwayat_diagnosa`.`hasil_radiologi` AS `hasil_radiologi`, `tbl_riwayat_diagnosa`.`hasil_catatan` AS `hasil_catatan`, `tbl_riwayat_diagnosa`.`tindakan` AS `tindakan`, `tbl_riwayat_diagnosa`.`berkas_tindakan` AS `berkas_tindakan` FROM (`v_antrian` left join `tbl_riwayat_diagnosa` on(`tbl_riwayat_diagnosa`.`id_antrian` = `v_antrian`.`id_antrian`)) ;
 
 --
 -- Indexes for dumped tables
@@ -424,6 +475,12 @@ ALTER TABLE `tbl_antrian`
 --
 ALTER TABLE `tbl_kecamatan`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeks untuk tabel `tbl_lab`
+--
+ALTER TABLE `tbl_lab`
+  ADD PRIMARY KEY (`id_lab`);
 
 --
 -- Indeks untuk tabel `tbl_pasien`
@@ -465,6 +522,12 @@ ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`id_user`);
 
 --
+-- Indeks untuk tabel `tbl_usg`
+--
+ALTER TABLE `tbl_usg`
+  ADD PRIMARY KEY (`id_usg`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -479,6 +542,12 @@ ALTER TABLE `tbl_antrian`
 --
 ALTER TABLE `tbl_kecamatan`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_lab`
+--
+ALTER TABLE `tbl_lab`
+  MODIFY `id_lab` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `tbl_pasien`
@@ -514,7 +583,13 @@ ALTER TABLE `tbl_transaksi_tindakan`
 -- AUTO_INCREMENT untuk tabel `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_usg`
+--
+ALTER TABLE `tbl_usg`
+  MODIFY `id_usg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
